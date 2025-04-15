67c6167d60e6cc89b2f9b8757a69cde1486f91fd
Kyle <81945767+Klyxen@users.noreply.github.com>
2025-04-15 11:27:17 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+#This is the main Script for Pin cracker
+pin = "284"
+
+#Checks the length of the pin
+pin_len = len(pin)
+
+#Finds the password and bases the length of the given pin
+for pin in range(10 ** pin_len):
+    guess = str(pin).zfill(pin_len) 
+    print(f"Trying PIN: {guess}")
+    if guess == pin:
+        print(f"PIN cracked! The correct PIN is: {guess}")
+        break
